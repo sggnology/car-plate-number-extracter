1d65335082d6b13149f8b9276f4eb41835a28bd0
sggnology <sggnology@gmail.com>
2023-08-27 16:27:19 +0900

    raw 텍스트로부터 차량 번호판 유형의 텍스트 추출후 출력

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import text_extract_car_plate as tecp
 
 reader = easyocr.Reader(['ko', 'en'],)
 raw_car_plates_candidates = reader.readtext('images/car-plate.jpg', detail=0)
-#
-# car_plate_candidates = tecp.extract_car_plates(raw_car_plates_candidates)
-#
-# print(tecp.extract_car_plates(car_plate_candidates))
+
+car_plate_candidates = tecp.extract_car_plates(raw_car_plates_candidates)
+
+print(tecp.extract_car_plates(car_plate_candidates))
 
 # # Press the green button in the gutter to run the script.
 # if __name__ == '__main__':
