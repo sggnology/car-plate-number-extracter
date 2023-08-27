e4fb451064011285eed69d9dc53d5c37fa576819
LuLuLaLaLa <haesung0728@gmail.com>
2023-08-27 16:23:54 +0900

    [init] 프로젝트 구성

---
+++ main.py
@@ -0,0 +1,13 @@
+import easyocr
+import text_extract_car_plate as tecp
+
+# reader = easyocr.Reader(['ko', 'en'],)
+# raw_car_plates_candidates = reader.readtext('images/car-plate.jpg', detail=0)
+#
+# car_plate_candidates = tecp.extract_car_plates(raw_car_plates_candidates)
+#
+# print(tecp.extract_car_plates(car_plate_candidates))
+
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print("start python ocr-tutorial")
